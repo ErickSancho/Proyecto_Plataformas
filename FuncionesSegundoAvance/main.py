085bcd0760f82508d4917b4cde94bc9653ed5bec
Juan10Di <73413205+Juan10Di@users.noreply.github.com>
2020-10-28 21:49:10 -0600

    Funciones para segundo avance
    Solo falta implementar funciones que hagan plots, pero de momentos todo los cálculos que se necesitas para una persona y para una empresa ya están.

---
+++ FuncionesSegundoAvance/main.py
@@ -0,0 +1,24 @@
+#coding = utf-8
+import funPersona
+import funEmpresa
+def main():
+    funEmpresa.setupEmpresa()
+    funEmpresa.crearCuentaEmpresa()
+    funEmpresa.ingresosEmpresa()
+    funEmpresa.gastosEmpresa()
+    funEmpresa.balanceEmpresa()
+    funEmpresa.setupEmpresa()
+    funEmpresa.crearCuentaEmpresa()
+    funEmpresa.ingresosEmpresa()
+    funEmpresa.gastosEmpresa()
+    funEmpresa.balanceEmpresa()
+    funEmpresa.setupEmpresa()
+    funEmpresa.crearCuentaEmpresa()
+    funEmpresa.ingresosEmpresa()
+    funEmpresa.gastosEmpresa()
+    funEmpresa.balanceEmpresa()
+
+    funEmpresa.impuestosEmpresa()
+
+if __name__ == "__main__":
+    main()
